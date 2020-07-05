f32fb9dd66bffd3effb17b58e6f082f140970cd8
Will Rochira <william.rochira@hotmail.co.uk>
2020-07-05 11:02:16 +0100

    Added example script and files

---
+++ example.py
@@ -0,0 +1,20 @@
+"""
+Copyright 2020 William Rochira at York Structural Biology Laboratory
+"""
+
+import os
+import sys
+import time
+
+from iris_validation import generate_report
+
+
+if __name__ == '__main__':
+    sys.dont_write_bytecode = True
+
+    latest_model_path = os.path.join('example_input', '2a0x_final.pdb')
+    previous_model_path = os.path.join('example_input', '2a0x_0cyc.pdb')
+    latest_reflections_path = os.path.join('example_input', '2a0x_final.mtz')
+    previous_reflections_path = os.path.join('example_input', '2a0x_0cyc.mtz')
+
+    generate_report(latest_model_path, previous_model_path, latest_reflections_path, previous_reflections_path, './example_report/', mode='')
