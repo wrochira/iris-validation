34add74dc0a8bcb954c932c075d4b8190b41b5e2
Will Rochira <william.rochira@hotmail.co.uk>
2020-11-03 15:48:53 +0000

    Reduced clipper submodule

--- iris_validation/clipper/__init__.py
+++ iris_validation/clipper/__init__.py
@@ -9,20 +9,5 @@ try:
     from clipper import *
     mode = 0
 except ImportError:
-    try:
-        # If on Windows, import local CP
-        import os
-        if os.name == 'nt':
-            import sys
-            if sys.version_info[0] == 2:
-                from py2.clipper import *
-            elif sys.version_info[0] == 3:
-                raise ImportError #from py3.clipper import *
-            mode = 0
-        # Else, import pip-installed CP
-        else:
-            from clipper_python._clipper import *
-            mode = 1
-    except ImportError:
-        print('WARNING: failed to import Clipper-Python, some functions will be unavailable')
-        mode = -1
+    print('WARNING: failed to import Clipper-Python, some functions will be unavailable')
+    mode = -1
